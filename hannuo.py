4fb9df4e8478d2635e51756c912a0848f32ff566
michael lan <apple@AppledeMacBook-Pro.local>
2020-04-03 21:21:06 +0800

    updated by the 2nd developer

--- hannuo.py
+++ hannuo.py
@@ -8,5 +8,11 @@ def move(n, a, b, c):
         move(n-1, a, c, b)# 子目标1
         move(1, a, b, c)# 子目标2	
         move(n-1, b, a, c)# 子目标3
+move(1,'A','B','C')
+move(2,'A','B','C')
+move(3,'A','B','C')
+move(4,'A','B','C')
 move(5,'A','B','C')
-move(10,'A','B','C')
+move(6,'A','B','C')
+move(15,'A','B','C')
+move(20,'A','B','C')
